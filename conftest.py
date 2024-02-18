1e8b22c58f60b6a1129cab90b5877ba502fd3df2
greylis <greylis@bluelight.inc>
2024-02-18 16:20:14 +0200

    Add server and fix URL

--- conftest.py
+++ conftest.py
@@ -3,8 +3,7 @@ import pytest
 
 @pytest.fixture
 def service_url():
-    # host = "127.0.0.1"
-    # port = "4000"
-    # url = f'ws://{host}:{port}'
-    url = 'ws://als.dedyn.io:4000'
+    host = "127.0.0.1"
+    port = "4000"
+    url = f'ws://{host}:{port}'
     return url
